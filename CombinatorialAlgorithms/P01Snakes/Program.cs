e502825f9be062a08745f03c6738f50cf58e1062
Tanya Milanova <tanyta78@gmail.com>
2019-02-07 10:53:54 +0200

    CombinatorialAlgo-hmw

--- CombinatorialAlgorithms/P01Snakes/Program.cs
+++ CombinatorialAlgorithms/P01Snakes/Program.cs
@@ -17,7 +17,11 @@
         {
             snakeLength = int.Parse(Console.ReadLine());
             currentSnake = new char[snakeLength];
-            CreateSnake(0, 0, 0, 'S');
+            //optimization
+            currentSnake[0] = 'S';
+            visited.Add(0 + " " + 0);
+            CreateSnake(1, 0, 1, 'R');
+
             Console.WriteLine($"Snakes count = {count}");
         }
 
@@ -26,12 +30,7 @@
             if (index >= snakeLength)
             {
                 var snake = new string(currentSnake);
-                if (!snakes.Contains(snake))
-                {
-                    Console.WriteLine(snake);
-                    count++;
-                    MarkSnake(snake);
-                }
+                MarkSnakeOpt(snake);
             }
             else
             {
@@ -51,10 +50,80 @@
             }
         }
 
+        private static void MarkSnakeOpt(string snake)
+        {
+            string flipped = FlipOpt(snake);
+            string reversed = ReverseOpt(snake);
+
+            if (!snakes.Contains(snake) &&
+                !snakes.Contains(flipped)) 
+            {
+                Console.WriteLine(snake);
+                count++;
+
+                snakes.Add(flipped);
+                snakes.Add(reversed);
+            }
+        }
+
+
+
+        private static char[] RotateOpt(char[] snake)
+        {
+            char[] rotated = snake;
+            while (rotated[1] != 'R')
+            {
+                for (int i = 0; i < snake.Length; i++)
+                {
+                    switch (snake[i])
+                    {
+                        case 'R': rotated[i] = 'D'; break;
+                        case 'D': rotated[i] = 'L'; break;
+                        case 'L': rotated[i] = 'U'; break;
+                        case 'U': rotated[i] = 'R'; break;
+                        default: rotated[i] = snake[i]; break;
+                    }
+                }
+            }
+
+            return rotated;
+        }
+
+        private static string ReverseOpt(string snake)
+        {
+            char[] reversed = new char[snakeLength];
+            reversed[0] = 'S';
+            for (int i = 1; i < snakeLength; i++)
+            {
+                reversed[i] = snake[snakeLength - i];
+            }
+            return new string(RotateOpt(reversed));
+        }
+
+        private static string FlipOpt(string snake)
+        {
+            char[] flipped = new char[snake.Length];
+            for (int i = 0; i < snake.Length; i++)
+            {
+                if (snake[i] == 'U')
+                {
+                    flipped[i] = 'D';
+                }
+                else if (snake[i] == 'D')
+                {
+                    flipped[i] = 'U';
+                }
+                else
+                {
+                    flipped[i] = snake[i];
+                }
+            }
+
+            return new string(RotateOpt(flipped));
+        }
+
         private static void MarkSnake(string snake)
         {
-
-
             string flipped = Flip(snake);
             string reversed = Reverse(snake);
             string flippedReversed = Reverse(flipped);
