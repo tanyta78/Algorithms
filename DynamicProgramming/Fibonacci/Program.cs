bdd73afb0671da6854b6361954d68880d7cc330d
Tanya Milanova <tanyta78@gmail.com>
2019-02-08 13:26:50 +0200

    DynamicProgramming -Fibonacci, Long Increasing Subsequence

---
+++ DynamicProgramming/Fibonacci/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Fibonacci
+{
+    using System;
+
+    public class Program
+    {
+        private static long[] memo;
+
+        public static void Main(string[] args)
+        {
+            var num = int.Parse(Console.ReadLine());
+            memo = new long[num + 1];
+            Console.WriteLine(FindFibonacciMember(num));
+        }
+
+        private static long FindFibonacciMember(int number)
+        {
+            if (memo[number] != 0)
+            {
+                return memo[number];
+            }
+
+            if (number <= 2)
+            {
+                memo[1] = 1;
+                memo[2] = 1;
+                return 1;
+            }
+
+            memo[number] = FindFibonacciMember(number - 1) + FindFibonacciMember(number - 2);
+
+            return memo[number];
+        }
+    }
+}
---
+++ DynamicProgramming/LongestIncreasingSubsequence/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace LongestIncreasingSubsequence
+{
+    using System;
+    using System.Linq;
+
+    public class Program
+    {
+        private static int[] sequence;
+        private static int[] memo;
+        private static int[] next;
+
+        public static void Main(string[] args)
+        {
+            sequence = Console.ReadLine()
+                              .Split(' ')
+                              .Select(int.Parse).ToArray();
+
+            memo = new int[sequence.Length];
+            next = new int[sequence.Length];
+
+            for (int i = 0; i < sequence.Length; i++)
+            {
+                LIS(i);
+            }
+
+
+            var maxLisCount = memo.Max();
+            var index = Array.IndexOf(memo, maxLisCount);
+            PrintLIS(index);
+            Console.WriteLine();
+        }
+
+        private static void PrintLIS(int index)
+        {
+            while (next[index] != index)
+            {
+                Console.Write(sequence[index] + " ");
+                index = next[index];
+            }
+            Console.Write(sequence[index]);
+
+        }
+
+        private static int LIS(int index)
+        {
+            if (memo[index] != 0)
+            {
+                return memo[index];
+            }
+
+            int maxLength = 1;
+            next[index] = index;
+            for (int i = index + 1; i < sequence.Length; i++)
+            {
+                if (sequence[index] < sequence[i])
+                {
+                    int length = LIS(i);
+                    if (length >= maxLength)
+                    {
+                        maxLength = length + 1;
+                        next[index] = i;
+                    }
+                }
+            }
+
+            memo[index] = maxLength;
+            return maxLength;
+
+        }
+    }
+}
