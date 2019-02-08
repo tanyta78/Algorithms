f1b3b53d56ef83386278881d5681ad3a43e20601
Tanya Milanova <tanyta78@gmail.com>
2019-02-08 22:20:23 +0200

    DynamicProgramming - Lab

---
+++ DynamicProgramming/MoveDownRightSum/Program.cs
@@ -0,0 +1,100 @@
+﻿namespace MoveDownRightSum
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        private static int[,] matrix;
+        private static int[,] sums;
+
+        public static void Main(string[] args)
+        {
+            //Given a matrix of N by M cells filled with positive integers, find the path from top left to bottom right with a highest sum of cells by moving only down or right.
+
+            int rows = int.Parse(Console.ReadLine());
+            int cols = int.Parse(Console.ReadLine());
+            ReadMatrix(rows, cols);
+            FillSumsMatrix(rows, cols);
+            PrintPath(rows - 1, cols - 1);
+        }
+
+        private static void PrintPath(int row, int col)
+        {
+            var path = new List<string>();
+            while (row + col > 0)
+            {
+                if (col == 0)
+                {
+                    path.Add($"[{row--}, {col}]");
+                    continue;
+                }
+
+                if (row == 0)
+                {
+                    path.Add($"[{row}, {col--}]");
+                    continue;
+                }
+
+                if (sums[row, col - 1] >= sums[row - 1, col])
+                {
+                    path.Add($"[{row}, {col--}]");
+                }
+                else
+                {
+                    path.Add($"[{row--}, {col}]");
+                }
+            }
+
+            path.Add($"[{row}, {col}]");
+            path.Reverse();
+
+            Console.WriteLine(string.Join(" ", path));
+        }
+
+        private static void FillSumsMatrix(int rows, int cols)
+        {
+            sums = new int[rows, cols];
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    var maxPrevSum = int.MinValue;
+
+                    if (col > 0 && sums[row, col - 1] > maxPrevSum)
+                    {
+                        maxPrevSum = sums[row, col - 1];
+                    }
+
+                    if (row > 0 && sums[row - 1, col] > maxPrevSum)
+                    {
+                        maxPrevSum = sums[row - 1, col];
+                    }
+
+                    sums[row, col] = matrix[row, col];
+
+                    if (maxPrevSum != int.MinValue)
+                    {
+                        sums[row, col] += maxPrevSum;
+                    }
+                }
+            }
+        }
+
+        private static void ReadMatrix(int rows, int cols)
+        {
+            matrix = new int[rows, cols];
+            for (int row = 0; row < rows; row++)
+            {
+                var currentRowElements = Console.ReadLine()
+                                                .Split(' ')
+                                                .Select(int.Parse).ToArray();
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = currentRowElements[col];
+                }
+            }
+        }
+    }
+}
---
+++ DynamicProgramming/RodCutting/Program.cs
@@ -0,0 +1,120 @@
+﻿namespace RodCutting
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        static int[] prices;
+        static int[] bestPrice;
+        static int[] bestIndex;
+
+        public static void Main(string[] args)
+        {
+            //Find the best way to cut up a rod with a specified length. You are also given to prices of all possible lengths starting from 0.
+
+            prices = Console.ReadLine()
+                                 .Split(' ')
+                                 .Select(int.Parse)
+                                 .ToArray();
+            var rodLength = int.Parse(Console.ReadLine());
+            bestPrice = new int[prices.Length];
+            bestIndex = new int[prices.Length];
+
+            PrintSolutionRecursive(rodLength);
+
+           //PrintSolution(rodLength);
+        }
+
+        private static void PrintSolutionRecursive(int rodLength)
+        {
+            int bestWay = CutRodRecursive(rodLength);
+            var result = new List<int>();
+
+            while (rodLength > 0)
+            {
+                int next = bestIndex[rodLength];
+                result.Add(next);
+                rodLength -= next;
+            }
+
+            Console.WriteLine(bestWay);
+            Console.WriteLine(String.Join(" ", result));
+        }
+
+        private static void PrintSolution(int rodLength)
+        {
+            int bestWay = CutRod(rodLength);
+            var result = new List<int>();
+
+            while (rodLength > 0)
+            {
+                int next = bestIndex[rodLength];
+                result.Add(next);
+                rodLength -= next;
+            }
+
+            Console.WriteLine(bestWay);
+            Console.WriteLine(String.Join(" ", result));
+           
+        }
+
+        private static int CutRod(int rodLength)
+        {
+            if (rodLength == 0)
+            {
+                return 0;
+            }
+
+            for (int i = 1; i <= rodLength; i++)
+            {
+                int currentBest = bestPrice[i];
+
+                for (int j = 1; j <= i; j++)
+                {
+                    currentBest =
+                        Math.Max(bestPrice[i], prices[j] + bestPrice[i - j]);
+                    if (currentBest > bestPrice[i])
+                    {
+                        bestPrice[i] = currentBest;
+                        bestIndex[i] = j;
+                    }
+                }
+            }
+
+            return bestPrice[rodLength];
+        }
+
+        private static int CutRodRecursive(int length)
+        {
+            if (bestPrice[length] != 0)
+            {
+                return bestPrice[length];
+            }
+
+            if (length == 0)
+            {
+                return 0;
+            }
+
+            int currentBest = 0;
+            int wholePart = length;
+
+            for (int i = 1; i <= length; i++)
+            {
+                int currentPrice = prices[i] + CutRodRecursive(length - i);
+
+                if (currentPrice > currentBest)
+                {
+                    wholePart = i;
+                    currentBest = currentPrice;
+                }
+            }
+
+            bestIndex[length] = wholePart;
+            bestPrice[length] = currentBest;
+            return bestPrice[length];
+        }
+    }
+}
