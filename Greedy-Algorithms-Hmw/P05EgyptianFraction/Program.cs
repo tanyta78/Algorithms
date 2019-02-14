0f33be730ec3b2523c18feb57a34fdfff726770e
Tanya Milanova <tanyta78@gmail.com>
2019-02-14 14:42:21 +0200

    GreedyAlgoHmw - EgyptianFraction

--- Greedy-Algorithms-Hmw/P05EgyptianFraction/Program.cs
+++ Greedy-Algorithms-Hmw/P05EgyptianFraction/Program.cs
@@ -2,25 +2,46 @@
 {
     using System;
     using System.Linq;
+    using System.Text;
 
     public class Program
     {
         public static void Main(string[] args)
         {
-            var fraction = Console.ReadLine().Split('/').Select(int.Parse).ToArray();
-            var fractionNumerator = fraction[0];
-            var fractionDenominator = fraction[1];
+            var fraction = Console.ReadLine().Split('/').Select(long.Parse).ToArray();
+            var fractionNumerator = fraction[0];//43
+            var fractionDenominator = fraction[1];//48
 
             if (fractionDenominator <= fractionNumerator)
             {
                 Console.WriteLine($"Error (fraction is equal to or greater than 1)");
+
             }
-
-            Console.WriteLine($"{fractionNumerator}/{fractionDenominator} = ");
-
-            while (true)
+            else
             {
-               
+                Console.Write($"{fractionNumerator}/{fractionDenominator} = ");
+                var currentDenominator = 2;
+                var result = new StringBuilder();
+                // 43/48 to 1/2
+                while (fractionNumerator > 0)
+                {
+                    var equatedFractionNumerator = fractionNumerator * currentDenominator;
+                    var equatedPartNumerator = fractionDenominator;
+
+                    if (equatedPartNumerator > equatedFractionNumerator)
+                    {
+                        currentDenominator++;
+                        continue;
+                    }
+
+                    fractionNumerator = equatedFractionNumerator - equatedPartNumerator;
+                    fractionDenominator *= currentDenominator;
+                    result.Append($"1/{currentDenominator} + ");
+                    currentDenominator++;
+                }
+
+                result.Remove(result.Length - 3, 2);
+                Console.WriteLine(result);
             }
         }
     }
