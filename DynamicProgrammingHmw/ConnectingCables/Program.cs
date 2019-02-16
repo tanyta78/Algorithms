d929a541138150a5f939ea64bfd1e99cb8e6a3a8
Tanya Milanova <tanyta78@gmail.com>
2019-02-16 21:59:17 +0200

    DPHmw- ConnectingCables, EditStringsCost

---
+++ DynamicProgrammingHmw/ConnectingCables/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace ConnectingCables
+{
+    using System;
+    using System.Linq;
+
+    public class Program
+    {
+        private static int[,] possibleCounts;
+
+        public static void Main(string[] args)
+        {
+            var cablesPermutation = Console.ReadLine()
+                                           .Split(' ')
+                                           .Select(int.Parse)
+                                           .ToArray();
+            var cablesOrdered = cablesPermutation.OrderBy(c => c).ToArray();
+
+            int result = FindMaxConnections(cablesPermutation, cablesOrdered);
+
+            Console.WriteLine($"Maximum pairs connected: {result}");
+        }
+
+        private static int FindMaxConnections(int[] side1, int[] side2)
+        {
+            int length = side1.Length;
+            possibleCounts = new int[length + 1, length + 1];
+
+            for (int i = 1; i <= length; i++)
+            {
+                for (int j = 1; j <= length; j++)
+                {
+                    if (side1[i - 1] == side2[j - 1])
+                    {
+                        possibleCounts[i, j] = possibleCounts[i - 1, j - 1] + 1;
+                    }
+                    else
+                    {
+                        possibleCounts[i, j] = Math.Max(possibleCounts[i - 1, j], possibleCounts[i, j - 1]);
+                    }
+                }
+            }
+
+            return possibleCounts[length, length];
+        }
+    }
+}
---
+++ DynamicProgrammingHmw/MinimumEditDistance/Program.cs
@@ -0,0 +1,117 @@
+﻿namespace MinimumEditDistance
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        private static int replaceCost;
+        private static int insertCost;
+        private static int deleteCost;
+        private static int[,] dp;
+
+       public static void Main(string[] args)
+       {
+           //only allowed to modify s1
+           //The goal is to find the sequence of operations which will produce s2 from s1 with minimal cost.
+           replaceCost = int.Parse(Console.ReadLine().Split(' ').ToArray()[2]);
+           insertCost = int.Parse(Console.ReadLine().Split(' ').ToArray()[2]);
+           deleteCost = int.Parse(Console.ReadLine().Split(' ').ToArray()[2]);
+
+           var s1 = Console.ReadLine().Split(' ').ToArray()[2];
+           var s2 = Console.ReadLine().Split(' ').ToArray()[2];
+
+           dp = new int [s1.Length + 1, s2.Length + 1];
+
+           for (int colIndex = 1; colIndex <= s2.Length; colIndex++)
+           {
+               dp[0, colIndex] = dp[0, colIndex - 1] + insertCost;
+           }
+
+           for (int row = 1; row <= s1.Length; row++)
+           {
+               dp[row, 0] = dp[row - 1, 0] + deleteCost;
+           }
+
+           for (int row = 1; row <= s1.Length; row++)
+           {
+               for (int col = 1; col <= s2.Length; col++)
+               {
+                   if (s1[row-1]==s2[col-1])
+                   {
+                       dp[row, col] = dp[row - 1, col - 1];
+                   }
+                   else
+                   {
+                       var delete = dp[row - 1, col] + deleteCost;
+                       var insert = dp[row, col - 1] + insertCost;
+                       var replace = dp[row - 1, col - 1]+replaceCost;
+
+                       dp[row, col] = Math.Min(delete, Math.Min(insert, replace));
+                   }
+               }
+           }
+
+          
+
+           PrintResult(s1,s2);
+       }
+
+       private static void PrintResult(string s1, string s2)
+       {
+           Console.WriteLine($"Minimum edit distance:{dp[s1.Length,s2.Length]}");
+
+           var resultOperations = new Stack<string>();
+           int fIndex = s1.Length;
+           int sIndex = s2.Length;
+           while (fIndex > 0 && sIndex > 0)
+           {
+               if (s1[fIndex - 1] == s2[sIndex - 1])
+               {
+                   fIndex--;
+                   sIndex--;
+               }
+               else
+               {
+                   int replace = dp[fIndex - 1, sIndex - 1] + replaceCost;
+                   int delete = dp[fIndex - 1, sIndex] + deleteCost;
+                   int insert = dp[fIndex, sIndex - 1] + insertCost;
+                   if (replace <= delete && replace <= insert)
+                   {
+                       resultOperations.Push($"REPLACE({fIndex - 1}, {s2[sIndex - 1]})");
+                       fIndex--;
+                       sIndex--;
+                   }
+                   else if (delete < replace && delete < insert)
+                   {
+                       resultOperations.Push($"DELETE({fIndex - 1})");
+                       fIndex--;
+                   }
+                   else
+                   {
+                       resultOperations.Push($"INSERT({sIndex - 1}, {s2[sIndex - 1]})");
+                       sIndex--;
+                   }
+               }
+           }
+
+           while (fIndex > 0)
+           {
+               resultOperations.Push($"DELETE({fIndex - 1})");
+               fIndex--;
+           }
+
+           while (sIndex > 0)
+           {
+               resultOperations.Push($"INSERT({sIndex - 1}, {s2[sIndex - 1]})");
+               sIndex--;
+           }
+
+           foreach (string resultOperation in resultOperations)
+           {
+               Console.WriteLine(resultOperation);
+           }
+       }
+    }
+}
