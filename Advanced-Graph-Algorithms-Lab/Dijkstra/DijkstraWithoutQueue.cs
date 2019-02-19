a1e601a3e68ca49adb3faca3a6e448c49e8cacbe
Tanya Milanova <tanyta78@gmail.com>
2019-02-19 14:25:30 +0200

    GraphsAlgoLab - Dijkstra

--- Advanced-Graph-Algorithms-Lab/Dijkstra/DijkstraWithoutQueue.cs
+++ Advanced-Graph-Algorithms-Lab/Dijkstra/DijkstraWithoutQueue.cs
@@ -7,7 +7,73 @@
     {
         public static List<int> DijkstraAlgorithm(int[,] graph, int sourceNode, int destinationNode)
         {
-            throw new NotImplementedException();
+            var n = graph.GetLength(0);
+
+            //Initialize distance[]
+            var distance = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                distance[i] = int.MaxValue;
+            }
+
+            distance[sourceNode] = 0;
+
+            var used = new bool[n];
+            var prev = new int?[n];
+
+            //Find nearest unvisited node from the source
+            while (true)
+            {
+                var minDistance = int.MaxValue;
+                int minNode = 0;
+                for (int node = 0; node < n; node++)
+                {
+                    if (!used[node] && distance[node]<minDistance)
+                    {
+                        minDistance = distance[node];
+                        minNode = node;
+                    }
+                }
+
+                if (minDistance==int.MaxValue)
+                {
+                    break;
+                }
+
+                used[minNode] = true;
+
+                //Improve shortest distance
+                for (int node = 0; node < n; node++)
+                {
+                    if (graph[minNode,node]>0)
+                    {
+                        var newDistance = minDistance+graph[minNode,node];
+
+                        if (newDistance<distance[node])
+                        {
+                            distance[node] = newDistance;
+                            prev[node] = minNode;
+                        }
+                    }
+                }
+            }
+            if(distance[destinationNode] == int.MaxValue)
+            {
+                return null;
+            }
+
+            var path = new List<int>();
+            int? currentNode = destinationNode;
+            while(currentNode != null)
+            {
+                path.Add(currentNode.Value);
+                currentNode = prev[currentNode.Value];
+            }
+
+            path.Reverse();
+
+            return path;
+           
         }
     }
 }
--- Advanced-Graph-Algorithms-Lab/DijkstraPriorityQueue/DijkstraWithPriorityQueue.cs
+++ Advanced-Graph-Algorithms-Lab/DijkstraPriorityQueue/DijkstraWithPriorityQueue.cs
@@ -7,7 +7,62 @@
     {
         public static List<int> DijkstraAlgorithm(Dictionary<Node, Dictionary<Node, int>> graph, Node sourceNode, Node destinationNode)
         {
-            throw new NotImplementedException();
+
+            var previous = new int?[graph.Count];
+            var visited = new bool[graph.Count];
+            var priorityQueue = new PriorityQueue<Node>();
+
+            foreach (var kvp in graph)
+            {
+                kvp.Key.DistanceFromStart = Double.PositiveInfinity;
+            }
+
+            sourceNode.DistanceFromStart = 0;
+            priorityQueue.Enqueue(sourceNode);
+
+            while (priorityQueue.Count > 0)
+            {
+                var smallest = priorityQueue.ExtractMin();
+
+                if (smallest == destinationNode)
+                {
+                    break;
+                }
+
+                foreach (var node in graph[smallest])
+                {
+                    if (!visited[node.Key.Id])
+                    {
+                        priorityQueue.Enqueue(node.Key);
+                        visited[node.Key.Id] = true;
+                    }
+
+                    var distance = smallest.DistanceFromStart + node.Value;
+
+                    if (distance < node.Key.DistanceFromStart)
+                    {
+                        node.Key.DistanceFromStart = distance;
+                        previous[node.Key.Id] = smallest.Id;
+                        priorityQueue.DecreaseKey(node.Key);
+                    }
+                }
+            }
+
+            if (double.IsInfinity(destinationNode.DistanceFromStart))
+            {
+                return null;
+            }
+
+            var path=new List<int>();
+            int? current = destinationNode.Id;
+            while (current!=null)
+            {
+                path.Add(current.Value);
+                current = previous[current.Value];
+            }
+
+            path.Reverse();
+            return path;
         }
     }
 }
