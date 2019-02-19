49f3b0407518a3444bdf1206bb10964fef2a63f9
Tanya Milanova <tanyta78@gmail.com>
2019-02-19 22:27:05 +0200

    GraphalgoLab - articulationPoint , MaxFlow

--- Advanced-Graph-Algorithms-II-Lab/ArticulationPoints/ArticulationPoints.cs
+++ Advanced-Graph-Algorithms-II-Lab/ArticulationPoints/ArticulationPoints.cs
@@ -3,8 +3,71 @@ using System.Collections.Generic;
 
 public class ArticulationPoints
 {
+    private static List<int>[] graph;
+    private static bool[] visited;
+    private static int[] depths;
+    private static int[] lowpoint;
+    private static int?[] parent;
+
+    private static List<int> articulationPoints;
+
+
     public static List<int> FindArticulationPoints(List<int>[] targetGraph)
     {
-        throw new NotImplementedException();
+        graph = targetGraph;
+        visited = new bool[targetGraph.Length];
+        depths = new int[targetGraph.Length];
+        lowpoint = new int[targetGraph.Length];
+        parent = new int?[targetGraph.Length];
+        articulationPoints = new List<int>();
+
+        for (int node = 0; node < graph.Length; node++)
+        {
+            if (!visited[node])
+            {
+                FindAP(node,0);
+            }
+        }
+
+        return articulationPoints;
+    }
+
+    private static void FindAP(int node, int depth)
+    {
+        visited[node] = true;
+        depths[node] = depth;
+        lowpoint[node] = depth;
+
+        int childCount = 0;
+        bool isArticulationPoint = false;
+
+        foreach (var child in graph[node])
+        {
+            if (!visited[child])
+            {
+                parent[child] = node;
+                FindAP(child, depth + 1);
+                childCount++;
+
+                if (lowpoint[child] >= depths[node])
+                {
+                    isArticulationPoint = true;
+                }
+
+                lowpoint[node] = Math.Min(lowpoint[node], lowpoint[child]);
+
+            }
+            else if (child != parent[node])
+            {
+                lowpoint[node] = Math.Min(lowpoint[node], depths[child]);
+            }
+        }
+
+        if ((parent[node] == null && childCount > 1) || (parent[node] != null && isArticulationPoint))
+        {
+            articulationPoints.Add(node);
+        }
+
+
     }
 }
